8ae9f545f0ebe07b27e2adae27a51c48d8476c89
Diloretodg <diloretodg74122@gmail.com>
2018-08-24 19:00:42 -0400

    setting up trivia question display in js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,89 +1,85 @@
 var correct = 0;
 var incorrect = 0;
 var timer = 20;
+var gameStart = false;
 
 
 
 
-var questions ={
+var questions = [{
     q1: { 
-        q:{},
-        a:{},
-        b:{},
-        c:{},
-        d:{},
+        q: "this is the question",
+        a: ["this an answer", "this is another", "this one is correct", "this one is not"],
+        gif: url="this is the question gif",
+        rightAnswer: 2,
     },
     q2: {
-        q:{},
-        a:{},
-        b:{},
-        c:{},
-        d:{},
+        q: "this is the question",
+        a: ["this an answer", "this is another", "this one is correct", "this one is not"],
+        gif: url="this is the question gif",
+        rightAnswer: 2,
     },
     q3: {
-        q:{},
-        a:{},
-        b:{},
-        c:{},
-        d:{},
+        q: "this is the question",
+        a: ["this an answer", "this is another", "this one is correct", "this one is not"],
+        gif: url="this is the question gif",
+        rightAnswer: 2,
     },
     q4: {
-        q:{},
-        a:{},
-        b:{},
-        c:{},
-        d:{},
+        q: "this is the question",
+        a: ["this an answer", "this is another", "this one is correct", "this one is not"],
+        gif: url="this is the question gif",
+        rightAnswer: 2,
     },
     q5: {
-        q:{},
-        a:{},
-        b:{},
-        c:{},
-        d:{},
+        q: "this is the question",
+        a: ["this an answer", "this is another", "this one is correct", "this one is not"],
+        gif: url="this is the question gif",
+        rightAnswer: 2,
     },
     q6: {
-        q:{},
-        a:{},
-        b:{},
-        c:{},
-        d:{},
+        q: "this is the question",
+        a: ["this an answer", "this is another", "this one is correct", "this one is not"],
+        gif: url="this is the question gif",
+        rightAnswer: 2,
     },
     q7: {
-        q:{},
-        a:{},
-        b:{},
-        c:{},
-        d:{},
+        q: "this is the question",
+        a: ["this an answer", "this is another", "this one is correct", "this one is not"],
+        gif: url="this is the question gif",
+        rightAnswer: 2,
     },
     q8: {
-        q:{},
-        a:{},
-        b:{},
-        c:{},
-        d:{},
+        q: "this is the question",
+        a: ["this an answer", "this is another", "this one is correct", "this one is not"],
+        gif: url="this is the question gif",
+        rightAnswer: 2,
     },
     q9: {
-        q:{},
-        a:{},
-        b:{},
-        c:{},
-        d:{},
+        q: "this is the question",
+        a: ["this an answer", "this is another", "this one is correct", "this one is not"],
+        gif: url="this is the question gif",
+        rightAnswer: 2,
     },
     q10: {
-        q:{},
-        a:{},
-        b:{},
-        c:{},
-        d:{},
+        q: "this is the question",
+        a: ["this an answer", "this is another", "this one is correct", "this one is not"],
+        gif: url="this is the question gif",
+        rightAnswer: 2,
     }
-}
+}];
+
+
 
 $("#start-button").click(function () {
     startGame();
 });
 
 function startGame() {
-
+    gameStart = true;
+    console.log("the game started");
+    pickQuestion();
+    
 }
 
 function questionTimer() {
--- assets/javascript/scratchpad.js
+++ assets/javascript/scratchpad.js
@@ -1,19 +1,31 @@
-function triviaQuestion(q, a,) {
-    
-}
+    function triviaGame() {
+        var correct = 0;
+        var incorrect = 0;
+        var timer = 20;
+        var questionsAsked = 0;
+        
+        function questionDisplay(x) {
+            return questions[x];
+            $("#trivia-question").text(this.q);
+            for (var i = 0; i < this.a.length; i ++) {
+                var newAwnswerDiv = $("<div>");
+                newAwnswerDiv.text(this.a[i]);
+                $("#trivia-answers").append(newAwnswerDiv);
+            };
+            questionsAsked ++;
+        };
 
-var questions ={
-    q1: { 
-        q: "this is the question",
-        a: ["this an answer", "this is another", "this one is correct", "this one is not"],
-        gif: url="this is the question gif",
-        rightAnswer: 2,
-    },
-    q2: {
-        q:{},
-        a:{},
-        b:{},
-        c:{},
-        d:{},
+        function currentQuestion(x) {
+            questionDisplay(questions[x]);
+        };
+
+        var questions ={
+            q1: { 
+                q: "this is the question",
+                a: ["this an answer", "this is another", "this one is correct", "this one is not"],
+                gif: url="this is the question gif",
+                rightAnswer: 2,
+            },
+        }
+    
     }
-}
